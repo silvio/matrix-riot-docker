53e28bde02f4ed0810d969c2cfc0556a61c910f7
Silvio Fricke <silvio.fricke@gmail.com>
2017-08-24 10:09:56 +0200

    push to dockerhub with `latest` tag
    Signed-off-by: Silvio Fricke <silvio.fricke@gmail.com>

--- misc/deploy.sh
+++ misc/deploy.sh
@@ -2,5 +2,5 @@
 # vim: set expandtab sw=4 ts=4 :
 
 docker login -u "${DOCKER_USER}" -p "${DOCKER_PASSWORD}"
-docker tag image silviof/matrix-riot-docker
-docker push silviof/matrix-riot-docker
+docker tag image silviof/matrix-riot-docker:latest
+docker push silviof/matrix-riot-docker:latest
